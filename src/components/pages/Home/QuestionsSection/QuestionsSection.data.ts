4ae8b8e0b993aac3ab222dab4c848c9aa9a908d6
Yash Ambaskar <kimetsu119@gmail.com>
2023-02-09 19:05:39 +0000

    coimmit

--- src/components/pages/Home/QuestionsSection/QuestionsSection.data.ts
+++ src/components/pages/Home/QuestionsSection/QuestionsSection.data.ts
@@ -10,35 +10,35 @@ export const data: Question[] = [
     id: 1,
     title: "How to get a tax deduction",
     description:
-      "Чтобы вернуть часть денег за обучение в автошколе, необходимо иметь постоянную регистрацию и официальное трудоустройство. Подробнее можно уточнить в Федеральной налоговой службе.",
+      "To return part of the money for training at a driving school, you must have permanent registration and official employment. For more information, please contact the Federal Tax Service.",
     isOpen: false
   },
   {
     id: 2,
-    title: "Сколько раз в неделю проходят занятия",
+    title: "How many times a week are classes",
     description:
-      "Учебный график подбирается индивидуально от потребностей ученика.",
+      "The study schedule is selected individually according to the needs of the student.",
     isOpen: false
   },
   {
     id: 3,
-    title: "Где оформить медицинскую справку?",
+    title: "Where can I get a medical certificate?",
     description:
-      "Чтобы оформить справку для получения или замены водительских прав, вам нужно будет пройти обследование врачом-психиатром-наркологом и врачом-психиатром в наркологических и психоневрологических диспансерах города проживания.",
+      "In order to issue a certificate for obtaining or replacing a driver's license, you will need to be examined by a psychiatrist-narcologist and a psychiatrist in narcological and neuropsychiatric clinics in the city of residence.",
     isOpen: false
   },
   {
     id: 4,
-    title: "Кому запрещено водить автомобиль?",
+    title: "Who is not allowed to drive?",
     description:
-      "Вождению припятствуют шизофрения, аффективные расстройства, умственная отсталось, эпилепсия, слепота обоих глаз.",
+      "Driving is hindered by schizophrenia, affective disorders, mental retardation, epilepsy, blindness in both eyes.",
     isOpen: false
   },
   {
     id: 5,
-    title: "Сколько стоит пересдать экзамен в автошколе? ",
+    title: "How much does it cost to retake an exam at a driving school? ",
     description:
-      "Стоимость пересдачи экзамена варируется в зависимости от категории. Цена не превышает 4 000 рублей.",
+      "The cost of retaking the exam varies depending on the category. The price does not exceed 4,000 Rupees.",
     isOpen: false
   }
 ];
